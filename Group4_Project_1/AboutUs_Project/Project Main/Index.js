b28595fcec6becb4a230ae629531e852d3d9da1e
chagz82 <118233513+chagz82@users.noreply.github.com>
2022-12-05 06:56:19 -0600

    update

--- Group4_Project_1/AboutUs_Project/Project Main/Index.js
+++ Group4_Project_1/AboutUs_Project/Project Main/Index.js
@@ -1,5 +1,7 @@
+//    <!------------------------------------------------------------------>
+//    <!----------------------------Log In Codes------------------------------->
+//    <!------------------------------------------------------------------>
 //validation form 
-
 //email.addEventlistener('textInput',email_Verify);
 //password.addEventlistener('textInput',pass_Verify);
 function validate(){
@@ -14,6 +16,46 @@ function validate(){
     }
     else if (password == ""){
         alert("Enter your password")
+        return false;
+    }
+}
+   
+
+    function email_Verify(){
+        if (email.value.length >=8){
+            email.style.border ="1px solid silver";
+            email_error.style.display = "none";
+            return true;
+        }
+    }
+
+   function pass_Verify(){
+        if (password.value.length >=5){
+            password.style.border ="1px solid silver";
+            pass_error.style.display = "none";
+            return true;
+        }
+   }
+
+
+//    <!------------------------------------------------------------------>
+//    <!----------------------------Sign Up------------------------------->
+//    <!------------------------------------------------------------------>
+   //validation form 
+//email.addEventlistener('textInput',email_Verify);
+//password.addEventlistener('textInput',pass_Verify);
+function validate(){
+    let email = document.forms["form"]["email"].value;
+    let password = document.forms["form"]["password"].value;
+
+    //let email_error = document.getElementById('email_error');
+    //let pass_error = document.getElementById('pass_error');
+    if (email == ""){
+        alert("Enter your Email Address")
+        return false;
+    }
+    else if (password == ""){
+        alert("Put your password")
         return false;
     }
 }
