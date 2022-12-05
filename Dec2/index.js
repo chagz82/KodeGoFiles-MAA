4fb3b9782da6162c6ac5a2c12f0805cf890dbcbd
chagz82 <118233513+chagz82@users.noreply.github.com>
2022-12-05 05:18:45 -0600

    update

---
+++ Dec2/index.js
@@ -0,0 +1,5 @@
+if (integer=>0 || integer<5) {
+    printValue="Number is less than 5.;
+}
+else(exit)
+
---
+++ Dec_1/index.js
@@ -0,0 +1,55 @@
+// Display functions
+// alert("Welcome to my world.");
+// console.log("Hello Console!")
+
+
+var person = "Mark"; //String data type/ literal string variable
+console.log(person);
+
+let age = 40;
+    age=38;
+console.log(age);
+
+const birthplace = "Davao City";
+console.log(birthplace);
+
+// Data types
+const firstname = "Mark";
+const lastname = "Artiaga";
+//Backtick
+const message = `Mark Artiaga
+This is another line
+                One more line
+`; //Backtick allows developer to use another line 
+console.log(message);
+
+const grade = 100; // Numeric data type
+const average = 96.5;    
+const passed=true; //boolean (true or false) values
+const value=null; //null
+const undef=undefined; //undefined
+
+
+// console.log(firstname, lastname);
+// console.log(typeof firstname);
+
+
+const x=33;
+const y=10;
+const z=x+y;
+const fName = "Mark";
+
+console.log("Result: ", x + y);
+console.log(z);
+console.log("Hello " + fName);
+
+console.log(`Result: ${z}`);
+console.log(`Subtraction: ${x-y}`);
+console.log(`Multiplication: ${x*y}`);
+console.log(`Division:${x/y}`);
+console.log(`Modulo: ${x%y}`);
+
+let counter=10;
+
+console.log(`Increament:  ${++counter}`);
+console.log(`Decreament: ${--counter}`);
